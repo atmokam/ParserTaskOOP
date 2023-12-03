7f640c5879901119c12c405b7345f03201b8f9ba
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-04 03:13:06 +0400

    Update Application.cpp
    Comments

--- ShapeParser/Application/Application.cpp
+++ ShapeParser/Application/Application.cpp
@@ -16,6 +16,8 @@ Application& Application::getInstance() {
 
 void Application::run(int count, char* args[]) {
     director = std::make_shared<Director>();
+    // also you need to create blank document here, document also held by app
+    // and process arguments, if path is specified automatically load the document
     controller = std::make_unique<CLIController>(count, args);
     controller->runProgram();
 }

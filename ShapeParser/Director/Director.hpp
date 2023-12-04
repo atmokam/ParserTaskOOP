b52df3228ed9884e3b1d19f11741e47d877aba1e
Tigran Khachakranc <tigrankhachakranc@users.noreply.github.com>
2023-12-04 04:02:16 +0400

    Update Director.hpp
    Comment

--- ShapeParser/Director/Director.hpp
+++ ShapeParser/Director/Director.hpp
@@ -8,14 +8,18 @@
 
 
 class Director : public IDirector {
+    // [TK] Document creation belongs to the application, director simply holds additional reference to it!
     std::shared_ptr<IDocument> document;
     size_t currentSlideIndex = 0;
-    std::shared_ptr<Slide> currentSlide;  
+    // [TK] Don't keep twice same thing, get current slide by currentSlideIndex whenever needed 
+    std::shared_ptr<Slide> currentSlide;
+    // [TK] undoRedo should be siomple STL ciontainer, do not overdesing
     std::unique_ptr<UndoRedo> undoRedo; // stacks
 
 public:
     
     Director();
+    // [TK] Good!
     void doAction(std::shared_ptr<IAction> action) override;
     void clearUndoRedoStack() override;
     std::shared_ptr<IDocument>& getDocument() override;
@@ -23,9 +27,11 @@ public:
     std::shared_ptr<Slide> getCurrentSlide() override;
     void setCurrentSlideNumber(size_t currentSlideNumber) override;
     size_t getCurrentSlideNumber() override;
+    // [TK] Why we need this, UndoRedo is private internals of the director nobody should get it
     std::unique_ptr<UndoRedo> getUndoRedo() override;
     void undo() override;
     void redo() override;
+    // [TK] Why we need this methods?
     void nextSlide() override;
     void previousSlide() override;
 
